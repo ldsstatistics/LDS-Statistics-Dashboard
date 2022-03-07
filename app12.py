0a7c756013d033bd31cb89414f7c4e3c950d50b5
ldsstatistics <>
2022-03-07 16:07:21 -0700

    Markdown dollar sign

--- app12.py
+++ app12.py
@@ -25,13 +25,13 @@ def app():
     st.plotly_chart(fig, use_container_width=True)
 
     st.write('''
-        Brigham Young's death-year value ($1.6 million) is given as the formal appraisal of his estate, 
-        which was 88 percent higher than the value ultimately agreed upon. The valuation of $361170 was 
+        Brigham Young's death-year value (\$1.6 million) is given as the formal appraisal of his estate, 
+        which was 88 percent higher than the value ultimately agreed upon. The valuation of \$361170 was 
         a reduction demanded by John Taylor as new trustee-in-trust for the church, and was eventually 
         accepted by Young's litigious heirs. Taylor privately threatened to drop from the Quorum of the
         Twelve the three apostles who were the estate's formally appointed trustees if they did not agree
         to this compromise. Taylor's demand was 40 percent lower than Young's 1875 concession of being 
-        worth something under $600000 during his divorce proceedings with Ann Eliza Webb Young. \n
+        worth something under \$600000 during his divorce proceedings with Ann Eliza Webb Young. \n
         -- D. Michael Quinn, The Mormon Hierarchy: Wealth and Corporate Power, Appendix 4
     ''')
 
@@ -83,7 +83,7 @@ def app():
         Pacific Railroad, which he defined as personal income. Money he earned simultaneously as trustee-in-trust for the
         LDS Church from 1868-71 was first recorded in the annual report of March 1872. The jump in non-hierarchy average 
         income for 1871 was due solely to Salt Lake City's non-LDS banker and mining magnate Warren G. Hussey, whose annual
-        income increased from $10000 in 1870 to $261392 in 1871.\n
+        income increased from \$10000 in 1870 to \$261392 in 1871.\n
         -- D. Michael Quinn, The Mormon Hierarchy: Wealth and Corporate Power, Appendix 2
     ''')
 
@@ -91,10 +91,10 @@ def app():
         There were huge disparities from 1862 to 1871. Always at the top, Brigham Young's annual income started out as four 
         times that of his regular counselors, Heber C. Kimball and Daniel H. Wells, and at least nine times their income from 
         1866 onward. Only his counselor-son Joseph A. Young (secretly ordained in 1864 and never publicly acknowledged) had 
-        income anywhere close to the church president's, and that was only in 1866. Brigham Young's $10,000 income dwarfed 
-        apostle Wilford Woodruff's $380 in 1862, as well as apostle John Taylor's $330. Likewise for the next three years, 
+        income anywhere close to the church president's, and that was only in 1866. Brigham Young's \$10,000 income dwarfed 
+        apostle Wilford Woodruff's \$380 in 1862, as well as apostle John Taylor's \$330. Likewise for the next three years, 
         when apostolic incomes in the hundreds of dollars contrasted with Brigham's ten grand. By the time his annual income 
-        reached $111,081 in 1870 (equal to $1,911,000 in today's purchasing power), only one apostle had taxable income. That 
+        reached \$111,081 in 1870 (equal to \$1,911,000 in today's purchasing power), only one apostle had taxable income. That 
         disparity had nothing to do with Brigham Young's role as trustee-in-trust, which generated a separate income twenty-two 
         times lower than his personal income that year.\n
         Of further interest may be how the general authorities compared with Utah's fifteen highest income earners among Utah's 
@@ -106,9 +106,9 @@ def app():
     ''')
 
     st.write('''
-        About 95% of Utah's income-earners received an income of less than $600 per year, excluding, of course, 
-        their own farm and garden produce consumed during the year. Of those 5% who were above the $600 level, more than 70%, 
-        on the average, received incomes of less than $1,000, and more than 90% earned less than $2,000.\n
+        About 95% of Utah's income-earners received an income of less than \$600 per year, excluding, of course, 
+        their own farm and garden produce consumed during the year. Of those 5% who were above the \$600 level, more than 70%, 
+        on the average, received incomes of less than \$1,000, and more than 90% earned less than \$2,000.\n
         -- [Leonard J. Arrington, Taxable Income in Utah, 1862-1872](https://issuu.com/utah10/docs/volume_24_1956/s/95990)
     ''')
 
